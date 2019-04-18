63e02301d4e2bf18b8c16472a1f528fb7deba9f6
domen111 <domen111@gmail.com>
2019-04-17 18:53:32 -0700

    admin login

--- lab2/app.js
+++ lab2/app.js
@@ -1,20 +1,11 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser');
 const spawn = require('child_process').spawn;
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get(/\/*/, (req, res) => {
-    let hostUrl = req.get('host');
-    if (hostUrl != '10.42.0.1') {
-        res.redirect(302, 'http://10.42.0.1');
-    }
-    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(`${ip} is asking for wifi!`);
-    res.setHeader('Content-type', 'text/html');
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(cookieParser())
 
 app.post('/login', (req, res) => {
     console.log(req.body);
@@ -33,6 +24,43 @@ app.post('/login', (req, res) => {
     }
 });
 
+let cookieToken = 'P6wT`Tt7aJA/@j9#5oqi@';
+app.get('/admin', (req, res) => {
+    if (req.cookies.admin == cookieToken) {
+        res.setHeader('Content-type', 'text/html');
+        res.sendFile(__dirname + '/admin.html');
+    } else {
+        res.setHeader('Content-type', 'text/html');
+        res.sendFile(__dirname + '/admin-login.html');
+    }
+});
+
+app.post('/admin/login', (req, res) => {
+    console.log(req.body);
+    let name = req.body.name;
+    let password = req.body.password;
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ip);
+    if (name == 'domen' && password == 'OctupusTea') {
+        res.cookie('admin', cookieToken); // Not secure
+        res.location('/admin');
+        res.send('<h3> success </h3>');
+    } else {
+        res.send('<h3> wrong password </h3>');
+    }
+});
+
+app.get(/\/*/, (req, res) => {
+    let hostUrl = req.get('host');
+    console.log(hostUrl);
+    if (hostUrl != '10.42.0.1:8888') {
+        res.redirect(302, 'http://10.42.0.1:8888');
+    }
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(`${ip} is asking for wifi!`);
+    res.setHeader('Content-type', 'text/html');
+    res.sendFile(__dirname + '/index.html');
+});
 
 let port = 8888;
 app.listen(port);
