d93d267b12dab9d18d016a88fd84b988470b5743
OctupusTea <eland.tea@gmail.com>
2019-06-12 17:01:10 +0800

    Functionality: Remove Routing Rule

--- final/set-app-route.sh
+++ final/set-app-route.sh
@@ -1,6 +1,14 @@
+#! /usr/bin/env bash
+
 FWMARK=10
 TABLE=100
-GATEWAY=$1
 
-ip rule add fwmark $FWMARK table $TABLE 2> /dev/null
-ip route add default via $GATEWAY table $TABLE 2> /dev/null
+if [[ $1 == "remove" ]]; then
+	ip rule del fwmark $FWMARK table $TABLE
+	ip route del table 100 default
+else
+	GATEWAY=$1
+
+	ip rule add fwmark $FWMARK table $TABLE
+	ip route add default via $GATEWAY table $TABLE
+fi
