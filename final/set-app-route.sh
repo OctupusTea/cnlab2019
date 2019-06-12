cea0d9d2920d6c662938667d93a0987e3ec203b8
OctupusTea <eland.tea@gmail.com>
2019-06-12 20:41:59 +0800

    Set Route Using ifname

--- final/set-app-route.sh
+++ final/set-app-route.sh
@@ -3,11 +3,14 @@
 FWMARK=10
 TABLE=100
 
-if [[ $1 == "remove" ]]; then
+if [[ $1 == "--remove" ]]; then
 	ip rule del fwmark $FWMARK table $TABLE
 	ip route del table 100 default
 else
-	GATEWAY=$1
+	IFACE=$1
+
+	GATEWAY=$( route -n | grep -e "^0.0.0.0" | grep $1 | cut -c17-32 | uniq )
+	echo $GATEWAY
 
 	ip rule add fwmark $FWMARK table $TABLE
 	ip route add default via $GATEWAY table $TABLE
