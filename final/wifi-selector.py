19e0f71fed9ba5cbf2fd8ad7fccf0f9e7657047e
domen111 <domen111@gmail.com>
2019-06-12 05:53:33 -0700

    retry connecting wifi

--- final/wifi-selector.py
+++ final/wifi-selector.py
@@ -21,27 +21,33 @@ def wifi_connect(iface,ssid):
 	return process.returncode == 0
 
 def speedtest():
-	process = popen(["./speedtest.sh"])
+	process = popen(["sudo", "./speedtest.sh"])
 	stdout, errout = process.communicate()
 	result = json.loads(stdout)
+	print(result)
 	return result['download']
 
 if __name__ == '__main__':
 	config = configparser.ConfigParser()
 	config.read("config.ini")
+	num_retry = 5
+
 	best_ssid = ''
 	best_speed = 0
 	for ssid in config.sections():
-		print("Connecting to", ssid)
-		if wifi_connect("wlx74da38f8c758", config[ssid]['file2']):
-			print("Connected")
-			print("Start testing speed...")
-			speed = speedtest()
-			print(ssid + " speed = " + str(speed / 1024 / 1024) + " Mbps")
-			if speed > best_speed:
-				best_speed = speed
-				best_ssid = ssid
-		else:
-			print("Failed")
+		for _ in range(num_retry):
+			print("Connecting to", ssid)
+			if wifi_connect("wlx74da38f8c758", config[ssid]['file2']):
+				print("Connected")
+				popen(["sudo", "./set-app-route.sh", "wlx74da38f8c758"]).wait()
+				print("Start testing speed...")
+				speed = speedtest()
+				print(ssid + " speed = " + str(speed / 1024 / 1024) + " Mbps")
+				if speed > best_speed:
+					best_speed = speed
+					best_ssid = ssid
+				break
+			else:
+				print("Failed")
 	print("Best wifi is:", best_ssid)
-	wifi_connect("wlx74da38e6c42f", config[best_ssid]['file1'])
+	# wifi_connect("wlx74da38e6c42f", config[best_ssid]['file1'])
